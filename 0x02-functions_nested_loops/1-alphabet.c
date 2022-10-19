3d4a5a54e46fcfd59660f159bccc73c896de88d9
Dexxii <dimpho.masilo12@gmail.com>
2022-10-19 03:57:34 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	char la;
+
+	for (la - 'a'; la <= 'z'; la++)
+		-putchar(la);
+
+	_putchar('\n');
+}

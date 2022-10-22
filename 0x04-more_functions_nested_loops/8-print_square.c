2cad9a389e01720c174f8ca5387fa41445632607
Dexxii <dimpho.masilo12@gmail.com>
2022-10-22 01:57:22 -0700

    Task number 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the same
+ */
+void print_square(int size)
+{
+	if (size <= o)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+
+			for (i = 0; j < size; j++)
+			{
+			_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}

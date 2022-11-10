18eda3e3cfe5ba9eb4774e975f615ad48b06d2b6
Dexxii <dimpho.masilo12@gmail.com>
2022-11-10 08:27:36 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,10 +14,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
-		s1 = ' ';
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = ' ';
+		s2 = "";
 
 	while (s1[size1] != '\0')
 	{
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < size1; i++)
 	{
-		p[i] = s1[i];
+		p[i] = s2[i - size1];
 	}
 	p[i] = '\0';
 
